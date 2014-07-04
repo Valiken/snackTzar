e195b6fb5fd8a6af7c9afa62b28386acbf6b6afb
Brian Berg <dachsund@dslextreme.com>
2014-07-03 18:49:42 -0700

    Third Commit
    fixed several things, submit box now clears

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -7,30 +7,35 @@
  * # MainCtrl
  * Controller of the snackTzarApp
  */
+
 angular.module('snackTzarApp')
   .controller('MainCtrl', ["$scope", "FireBaseServ", function ($scope, FireBaseServ) {
     $scope.snackList = FireBaseServ;
-    console.log($scope.snackList);
-    $scope.addSnack = function (sn) {
+    
+    $scope.addSnack = function (sn, usr) {
       var name =  sn;
+      var user = usr.displayName;
       if (name != null) {
-        console.log(name);
-        $scope.snackList.$add({reqFulfilled: "no", snackName: name});
-        $scope.snack = '';
+        $scope.snackList.$add({reqFulfilled: "no", snackName: name, userName: user});
       }
       else {
         console.log("no snack name was entered");
       }
-    }
-
+    };
+    
+    $scope.reset = function () {
+      $scope.snack = "";
+    };
+    
     $scope.updateSnack = function (sn) {
       var name = sn;
       if (name != null) {
         $scope.snackList.$update({ snackname: name, reqFulfilled: "yes"});
       }
-    }
+    };
 
     $scope.removeSnack = function () {
 
-    } 
-  }]);
+    };
+  }
+]);
